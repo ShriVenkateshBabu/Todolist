d4af303b345638ce94c4afc001106faf2fd56c76
SHRI VENKATESH BABU B <shrivenkatesh679@gmail.com>
2024-11-11 20:36:06 +0530

    11-11-2024

--- src/Components/Content.jsx
+++ src/Components/Content.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { FaTrash } from "react-icons/fa";
 
+
 const Content = () => {
 
     let [items, setitmes] = useState([
@@ -11,10 +12,12 @@ const Content = () => {
       },
       {
         id:2,
-        item:"coding",
+        item:"coding venky",
         checked: false,
       }
     ])
+  
+    // console.log(items)
     function handleCheck(id){
       const newlist = items.map((itms)=>{
         if(itms.id===id)
@@ -25,26 +28,42 @@ const Content = () => {
         {
             return itms
         }
-      })  
+      }) 
+      localStorage.setItem("to-do-list",JSON.stringify(newlist)) 
       setitmes(newlist)
     }
-    
+    function handleclick(id){
+      const new_list = items.filter((itm)=>itm.id!=id)
+      localStorage.setItem("to-do-list", JSON.stringify(new_list))
+      setitmes(new_list);
+    }
+
     return (
       <div>
        <p>TO DO LIST</p>
+       {(items.length)? (
        <ul>
-        {items.map((itm)=>(
-          <li className='item' key={itm.id}>
+        {items.map((itm)=>{
+        //  let labelstyle ={}
+        //  if(itm.checked){
+        //   labelstyle={textDecoration:"line-through"}
+        //   }
+          return (<li className='item' key={itm.id}>
             <input type='checkbox'
             onChange={ () => handleCheck(itm.id) }
             checked = {itm.checked}
             />
-            <label>{itm.item}</label>
-             <button><FaTrash/></button>
-          </li>
-           
-        ))}
-       </ul>
+                       <label
+            style={itm.checked ? {textDecoration:"line-through"}:null}
+            onDoubleClick={() => handleCheck(itm.id)}
+            >{itm.item}</label>
+             <FaTrash 
+             onClick={()=> handleclick(itm.id)}
+             role='button'
+             />
+              </li>)
+})}
+       </ul>):<p>your list is empty</p>}
       </div>
     )
 }
