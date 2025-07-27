36b06c80b88d49ec88550c1ccd88155aeceab8b3
SHRI VENKATESH BABU B <shrivenkatesh679@gmail.com>
2025-07-27 23:27:35 +0530

    login_form

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,12 +1,17 @@
 import './login.scss'
 const Login = () => {
+  const loginBtn = (e) => {
+    window.location.href = '/'
+    e.preventDefault(); // Prevent the default form submission behavior
+  }
   return (
     <div className='login_container'>
-      <form className='login_form'>
-        <label htmlFor="username">Username:</label>
-        <input type="text" id="username" placeholder="Enter your username" />
-        <label htmlFor="password">password</label>
-        <input type="text" id="password" placeholder="Enter your password" />
+      <form className='login_form' type='submit'>
+        <label htmlFor="username">Username</label>
+        <input type="text" id="username" name='username' placeholder="Enter your username" />
+        <label htmlFor="password">Password</label>
+        <input type="password" id="password" name='password' placeholder="Enter your password" />
+        <button onClick={loginBtn} type='submit'>Login</button>
       </form> 
     </div>
   )
