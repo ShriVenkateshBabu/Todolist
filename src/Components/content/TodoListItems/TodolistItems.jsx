a8d54838e58e0fc7c0d4464e16fd33ad1d9f16c4
SHRI VENKATESH BABU B <shrivenkatesh679@gmail.com>
2025-08-01 13:23:47 +0530

    CRUD,Styling,CustomValidty

--- src/Components/content/TodoListItems/TodolistItems.jsx
+++ src/Components/content/TodoListItems/TodolistItems.jsx
@@ -3,21 +3,23 @@ import { TodoButton } from "../../../StyleComponents/Button";
 import { DataContext } from "../../../ContextAPI/UseContext.jsx";
 import { useContext } from "react";
 import ListItem from "./ListItem.jsx";
+import useTodoFunctions from "./TodoFunctions/TodoFunctions.jsx";
 const TodolistItems = () => {
-  const { listItems, TaskName, setTaskName, isToggle } =
+  const { listItems, TaskName, setTaskName, isToggle ,Inputref } =
     useContext(DataContext);
+  const {handleAddTask} = useTodoFunctions()
 
   return (
     <>
-      <label htmlFor="InputTask">Add Task</label>
       <input
         id="InputTask"
+        ref ={Inputref}
         type="text"
         onChange={(e) => setTaskName(e.target.value)}
         value={TaskName}
         placeholder="Enter the task"
       />
-      <TodoButton role="button" tabIndex={0}>
+      <TodoButton role="button" onClick={handleAddTask} tabIndex={0}>
         {isToggle ? "Save" : "Add Task"}
       </TodoButton>
       <ListItem list={listItems} />
